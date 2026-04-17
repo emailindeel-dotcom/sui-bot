fc9e49e7f8c065816c53a569052d12be4a62eb3e
emailindeel-dotcom <emailindeel@gmail.com>
2026-04-17 20:26:56 +0530

    Change trading symbol from SUIINR to B-SUI_INR

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def get_signature(payload):
 
 # Fetch candles
 def get_candles():
-    symbol = "SUIINR"
+    symbol = "B-SUI_INR"
 
     url = f"https://public.coindcx.com/market_data/candles?pair={symbol}&interval={BAR_INTERVAL}"
     response = requests.get(url)
